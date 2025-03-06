b0b73a3009dfaebc854219d96cc4d1a7452fde43
IlyaRogashev <150829978+IlyaRogashev@users.noreply.github.com>
2025-03-06 16:21:51 +0400

    Update main.go

--- calc_servise/cmd/agent_start/main.go
+++ calc_servise/cmd/agent_start/main.go
@@ -1,1 +1,37 @@
+package main
 
+import (
+    "context"
+    "log"
+    "os"
+    "strconv"
+    "time"
+
+)
+
+func main() {
+    serverURL := os.Getenv("SERVER_URL")
+    if serverURL == "" {
+        log.Fatalf("SERVER_URL environment variable is not set")
+    }
+
+    computingPowerStr := os.Getenv("COMPUTING_POWER")
+    if computingPowerStr == "" {
+        log.Fatalf("COMPUTING_POWER environment variable is not set")
+    }
+
+    computingPower, err := strconv.Atoi(computingPowerStr)
+    if err != nil {
+        log.Fatalf("Failed to parse COMPUTING_POWER: %v", err)
+    }
+
+    ctx, cancel := context.WithCancel(context.Background())
+    defer cancel()
+
+    agent := agent.NewAgent(ctx, computingPower, serverURL)
+
+    agent.Start()
+
+    log.Println("Press Ctrl+C to stop the agent.")
+    select {}
+}
