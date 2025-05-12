baba6736ca1b16be5ca83fa70bf65b106f75aa96
IlyaRogashev <150829978+IlyaRogashev@users.noreply.github.com>
2025-05-12 15:41:50 +0400

    Update main.go
    Я тут чуть опоздал, прошу понять и простить

--- calc_servise/main.go
+++ calc_servise/main.go
@@ -1,51 +1,23 @@
 package main
 
 import (
-    "context"
-    "log"
-    "net/http"
-    "os"
-    "os/signal"
-    "syscall"
-    "time"
+	"log"
+	"os"
 
+	"google.golang.org/grpc"
+	"github.com/IlyaRogashev/calc_go/calc_servise/application"
 )
 
 func main() {
-    ctx, cancel := context.WithCancel(context.Background())
-    defer cancel()
+	orchestrator := application.NewOrchestrator()
+	go func() {
+		if err := orchestrator.RunServer(); err != nil {
+			log.Fatalf("failed to run orchestrator: %v", err)
+		}
+	}()
 
-    orchestrator := orchestrator.NewOrchestrator(ctx)
+	agent := application.NewAgent()
+	agent.Run()
 
-    router := mux.NewRouter()
-
-    orchestrator.RegisterHandlers(router, orchestrator)
-
-    srv := &http.Server{
-        Addr:    ":8000",
-        Handler: router,
-    }
-
-    go orchestrator.Start()
-
-    go func() {
-        if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-            log.Fatalf("listen: %s\n", err)
-        }
-    }()
-
-    // Graceful shutdown
-    quit := make(chan os.Signal, 1)
-    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-    <-quit
-    log.Println("Shutting down server...")
-
-    orchestrator.Stop()
-
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancel()
-    if err := srv.Shutdown(ctx); err != nil {
-        log.Fatalf("server shutdown failed: %v", err)
-    }
-    log.Println("Server exited properly")
+	select {}
 }
